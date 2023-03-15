c8c26ff6ab7efd2d9ac68e797da5fcff2fc45508
Samuel Legrain <samuel.legrain@bstorm.be>
2023-03-15 14:49:00 +0100

    Correction + demo routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+import { UserLoginComponent } from './Users/Components/user-login/user-login.component';
+import { UserMainComponent } from './Users/Components/user-main/user-main.component';
+import { PageNotFoundComponent } from './Error/Components/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './Main/Components/main-content/main-content.component';
+import { UserSigninComponent } from './Users/Components/user-signin/user-signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Redirection page principale
+  {path:'', pathMatch: 'full', redirectTo :'home'},
+  //Définition de tout les components et de leur route
+  {path:'home',component:MainContentComponent},
+  {path:'user', component : UserMainComponent, children:[
+    //Redirection page d'inscription (le User-Main ne sert à rien sans les enfants...)
+    {path:'', pathMatch:'full', redirectTo:'signin'},
+    {path:'login', component : UserLoginComponent},
+    {path:'signin', component : UserSigninComponent}
+  ]},
+  //Erreurs
+  {path:'**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainContentComponent } from './Main/Components/main-content/main-content.component';
+import { PageNotFoundComponent } from './Error/Components/page-not-found/page-not-found.component';
+import { UserLoginComponent } from './Users/Components/user-login/user-login.component';
+import { UserSigninComponent } from './Users/Components/user-signin/user-signin.component';
+import { UserMainComponent } from './Users/Components/user-main/user-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent,
+    PageNotFoundComponent,
+    UserLoginComponent,
+    UserSigninComponent,
+    UserMainComponent
   ],
   imports: [
     BrowserModule,
